965c9d6e3d8b38da02a47c3fb02fd23984b0bffd
turbo <gymgym@88.com>
2023-07-11 15:27:24 +0800

    test: update Test.java

--- demo-client-core/src/main/java/io/github/kristineberg/Test.java
+++ demo-client-core/src/main/java/io/github/kristineberg/Test.java
@@ -3,5 +3,6 @@ package io.github.kristineberg;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test.java");
+        System.out.println("Demo Pr");
     }
 }
